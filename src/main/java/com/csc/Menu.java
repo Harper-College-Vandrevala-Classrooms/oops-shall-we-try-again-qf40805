7844a31629a24c2a0e56efccd9b52874cbbeb9a4
qf40805 <qf40805@mail.harpercollege.edu>
2024-09-03 13:03:27 -0500

    Fayaz Qureshi

--- src/main/java/com/csc/Menu.java
+++ src/main/java/com/csc/Menu.java
@@ -1,3 +1,48 @@
 package com.csc;
 
+import java.util.Scanner;
+
+public class Menu{
+    public static int menu(Integer min, Integer max, String prompt, String error){
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.print(prompt);
+            int input = scanner.nextInt();
+            boolean valid = true;
+            if(min != null && input < min){
+                valid = false;
+            }
+            if(max != null && input > max){
+                valid = false;
+            }
+            if(valid){
+                return input;
+            }else{
+                System.out.println(error);
+           }
+        }
+    }
+
+    public static void main(String[] args){
+        String prompt = "Enter a number: ";
+        String error = "Number is invalid ";
+
+        int result = Menu.menu(null, null, prompt, error);
+        System.out.println("No bounds: " + result);
+        result = Menu.menu(null, 10, prompt, error);
+        System.out.println("With bounds: " + result);
+        result = Menu.menu(0, null, prompt, error);
+        System.out.println("With bounds: " + result);
+        result = Menu.menu(0, 10, prompt, error);
+        System.out.println("With bounds: " + result);
+        result = Menu.menu(null, 10, prompt, error);
+        System.out.println("With bounds: " + result);
+        result = Menu.menu(-10, null, prompt, error);
+        System.out.println("With bounds: " + result);
+        result = Menu.menu(-10, 5, prompt, error);
+        System.out.println("With bounds: " + result);   
+}
+
+}
+
 // Your code goes here!
